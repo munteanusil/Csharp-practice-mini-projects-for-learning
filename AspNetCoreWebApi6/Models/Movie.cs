30757d18cb1c11ca2710f8cc58d0492afbf3fd2c
Munteanu Silviu <munteanusilviu1996@yahoo.com>
2023-03-13 13:25:05 +0200

    First commit for this repository

---
+++ AspNetCoreWebApi6/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCoreWebApi6.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } //The Id field is required by the database for the primary key.
+        public string? Title { get; set; }
+
+        public string? Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
---
+++ AspNetCoreWebApi6/MovieContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreWebApi6.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AspNetCoreWebApi6
+{
+    public class MovieContext:DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options): base(options) 
+        { 
+        }
+        DbSet<Movie> Movies { get; set; } = null;
+    }
+}
